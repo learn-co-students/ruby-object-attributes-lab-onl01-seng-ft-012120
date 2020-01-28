e66942879ea479dec0bfc132f5cf909e9649a952
Agnes Park <agnespark824@gmail.com>
2020-01-28 18:17:03 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(a_dogs_name)
+    @name = a_dogs_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(a_dogs_breed)
+    @breed = a_dogs_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
