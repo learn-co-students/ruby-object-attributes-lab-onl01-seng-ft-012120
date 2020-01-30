3c018fa99f96c4faff07668c998579b03033caa0
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-30 21:16:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  def name 
+    return @name
+  end
+  def breed=(dogs_breeds)
+    @breed = dogs_breeds
+  end
+  def breed
+    return @breed
+  end
+end
