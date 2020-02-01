959ab94a02c8e4df1fd35dc667a897643ceae8b4
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-02-01 04:50:00 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  #name setter and getter methods for Dog class
+  def name=(dog_name)
+   @name = dog_name
+  end
+  
+  def name 
+    @name
+  end
+  
+  #breed setter and getter methods for Dog class 
+   def breed=(dog_breed)
+     @breed = dog_breed
+   end 
+   
+   def breed 
+     @breed
+   end
+end 
